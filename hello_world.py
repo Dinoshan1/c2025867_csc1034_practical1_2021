cc5eecccb577b054824ea4052f685233d6a81c2b
c2025867 <c2025867@newcastle.ac.uk>
2022-09-29 14:22:48 +0100

    Adding some scenery

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,19 @@
-import sys
-import platform
+from direct. showbase.ShowBase import ShowBase
 
-print("hello")
+class MyApp(ShowBase):
 
-print(1, sys. version)
-print(2, platform.implementation)
-print(3, sys.executable)
+    def _init_(self):
+        ShowBase._init_(self)
+
+        # Load the environment model
+        self.scene = self.loader. loadModel("models/environment")
+        # Reparent the model to render.
+        self.scene. reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene. setPos(-8, 42, 0)
+
+app=MyApp()
+app.run()
+
+

508f96b7a7d901dca8a5e11c85f8a993496a346a
c2025867 <c2025867@newcastle.ac.uk>
2022-09-29 14:39:16 +0100

    Add 3D Panda

--- hello_world.py
+++ hello_world.py
@@ -13,6 +13,25 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene. setPos(-8, 42, 0)
 
+        # Add the spinCameraTask procedure to the task manager.
+        self. task.Mgr. add(self. spinCameraTask, "SpinCameraTask")
+
+        # Load and transform the panda actor.
+        self.pandaActor = Actor("models/ panda-model",
+                                {"walk": "models/panda-walk4"})
+        self. pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        # Loop its animation.
+        self.pandaActor.loop("walk")
+
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task) :
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self. camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
 app=MyApp()
 app.run()
 
